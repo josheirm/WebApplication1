7a6b46204daa1d450c3b226089dac5cfa00d093d
JoshuaE <15404@LAPTOP-ABMUOFI9>
2019-09-15 11:30:27 -0400

    added changeuser function

--- lobby/Site1.Master.cs
+++ lobby/Site1.Master.cs
@@ -5,6 +5,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+public class SessionTest
+{
+    public string GetSession(string SessionVariable)
+    {
+
+        Session["A"] = "";
+        return SessionVariable;
+
+    }
+}
+
+
+
 
 public class OneChatRoom
 {
@@ -35,6 +48,9 @@ namespace lobby
         //string b = a;
 
 
+        
+
+
         int rand = 0;
         List<OneChatRoom> list1 = new List<OneChatRoom>();
         
@@ -61,14 +77,16 @@ namespace lobby
         //}
         protected void Application_Start(object sender, EventArgs e)
         {
+            
             // Session["string1"] = rand.ToString();
 
 
-           
+
         }
 
         protected void Session_Start(object sender, EventArgs e)
         {
+
             
 
             //Label1.Text = "aa";// Application["randomnumber1"].ToString();
@@ -78,13 +96,19 @@ namespace lobby
         {
             if (Page.IsPostBack)
             {
+
             }
             else
             {
                 rand = new Random().Next(1, 5);
-                Application["randomnumber1"] = rand;
-                Application["whoseturnisit"] = 1;
-                Label1.Text = rand.ToString();
+                Application["randomnumber1"] = rand.ToString();
+
+
+                Session["MyUserNumber"] = "1";
+                
+                //player one goes first
+               
+                
                 //initial set
                 Application["Application_list1"] = list1;
                 Application["Application_list2"] = list2;
@@ -92,8 +116,10 @@ namespace lobby
                 Application["Application_list4"] = list4;
                 Application["Application_list5"] = list5;
 
-
-
+                Application["user1done"] = "1";
+                Application["user2done"] = "0";
+                Application["whoseturnisit"] = "1";
+                
 
             }
 
@@ -104,192 +130,192 @@ namespace lobby
 
 
         }
-        
 
+        //player one goes first
         protected void Button1_Click(object sender, EventArgs e)
         {
 
             //this is the main hack!
             //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+            //whosturnisit is : 1
 
-            Session["MyUserNumber"] = 1;
-            //RECEIVING MESSAGE if not the same
-            if (Session["MyUserNumber"] != Application["whoseturnisit"])
-            {
             
-              
+            //this goes second - user one done
+            
+            //receive code - user 1 goes first this is player 2
+            if (Session["MyUserNumber"] != Application["whoseturnisit"] )
+            {
 
-                    //TWO OF THESE!!!!! - determined by previous page - hack
-                    var roomnumber = 1;
-                    
-                    if (roomnumber == 1 )
-                    {
+                ////////////for testing
+                string msg = "1";
+                OneChatRoom message = new OneChatRoom(msg, "0", "0");
+                list1.Add(message);
+                Application["Application_list1"] = list1;
+                //list1.Add(message);
+                list1 = (List<OneChatRoom>)Application["Application_list1"];
+                //////////end testing
+
+
+                //TWO OF THESE!!!!! - determined by previous page - hack
+                var roomnumber = 1;
+
+                if (roomnumber == 1)
+                {
                     //the code is the number of the button sent to disable
                     var code = (List<OneChatRoom>)Application["Application_list1"];
                     list1.RemoveAt(0);
+                    //Session["thecode"] = code ;
+
                     //to use after
-                    Session["thecode"] = code;
+
                     //held in an application variable
                     Application["Application_list1"] = list1;
+                    Button6.Enabled = false;
+
 
                 }
-                    if (roomnumber == 2)
-                    {
+                else if (roomnumber == 2)
+                {
                     var code = (List<OneChatRoom>)Application["Application_list2"];
                     list2.RemoveAt(0);
-                    Session["thecode"] = code;
-                    Application["Application_list2"] = list1;
-                }
-                    if (roomnumber == 3)
-                    {
-                    var code = (List<OneChatRoom>)Application["Application_list3"];
-                    list3.RemoveAt(0);
-                    Session["thecode"] = code;
-                    Application["Application_list3"] = list1;
-                }
-                    if (roomnumber == 4)
-                    {
-                    var code = (List<OneChatRoom>)Application["Application_list4"];
-                    list4.RemoveAt(0);
-                    Session["thecode"] = code;
-                    Application["Application_list4"] = list1;
-                }
-                    if (roomnumber == 5)
-                    {
-                    var code = (List<OneChatRoom>)Application["Application_list5"];
-                    list5.RemoveAt(0);
-                    Session["thecode"] = code;
-                    Application["Application_list5"] = list1;
-                }
-                    
-                    //disable button of code sent
-
-                    if(Session["thecode"] == "1")
-                    {
-                         Button6.Enabled = false;
-                    }
-
-
-                if (Session["thecode"] == "2")
-                {
+                    //Session["thecode"] = code;
+                    Application["Application_list2"] = list2;
                     Button2.Enabled = false;
                 }
-
-                if (Session["thecode"] == "3")
+                else if (roomnumber == 3)
                 {
+                    var code = (List<OneChatRoom>)Application["Application_list3"];
+                    list3.RemoveAt(0);
+                    //Session["thecode"] = code;
+                    Application["Application_list3"] = list3;
                     Button3.Enabled = false;
                 }
-
-                if (Session["thecode"] == "4")
+                else if (roomnumber == 4)
                 {
+                    var code = (List<OneChatRoom>)Application["Application_list4"];
+                    list4.RemoveAt(0);
+                    //Session["thecode"] = code;
+                    Application["Application_list4"] = list4;
                     Button4.Enabled = false;
                 }
-
-                if (Session["thecode"] == "5")
+                else if (roomnumber == 5)
                 {
+                    var code = (List<OneChatRoom>)Application["Application_list5"];
+                    list5.RemoveAt(0);
+                    //Session["thecode"] = code;
+                    Application["Application_list5"] = list5;
                     Button5.Enabled = false;
                 }
 
-
-                //got code 
+               
+                               
                 // now switch users and return out so user sends next time
                 var temp = Application["whoseturnisit"];
-                if ((string)temp == "1")
+                if (temp == "1")
                 {
-                    Application["whoseturnisit"] = 0;
+                    Application["whoseturnisit"] = "0";
                 }
                 else
                 {
-                    Application["whoseturnisit"] = 1;
+                    Application["whoseturnisit"] = "1";
                 }
 
-                //exit receiving code
-                return;
-            }
-
-            ///////////////////////////////////////////////
-            //this here is send a message, above is receive
-            ///////////////////////////////////////////////
-            
-            //this is button one, really - user chose button one 
-
-            Button6.Enabled = false;
-            var  holder1 = Application["randomnumber1"];
-            
-            //you won got right pick
-            if (holder1 == "1")
-            {
-                //list1.Add(a);
-                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "alert('You Win!'); ", true);
-
-            }
-
-
-            //wrong pick - send message with code to disable
-            else
-            {
-
-                ///////////////////////////////////////////////////////////////////////
-                //this, below, a function//////////////////////////////////////////////
-                ///////////////////////////////////////////////////////////////////////
-
-                OneChatRoom message = new OneChatRoom("1", "0", "0");
-                //list1.Add(msg1);
-
-
-                //////////!!!!!!!!!!!!!!!!!!!!!TWO OF THESE!!!! forcing
-                //variable set at previous screen
-                var roomnumber = 1;
                 
 
-                //sends message
-                switch (roomnumber)
-                {
-                    case 1:
-                        list1.Add(message);
-                        Application["Application_list1"] = list1;
-                        break;
-
-                    case 2:
-                        list2.Add(message);
-                        Application["Application_list2"] = list2;
-                        break;
-                    case 3:
-                        list3.Add(message);
-                        Application["Application_list3"] = list3;
-                        break;
-                    case 4:
-                        list4.Add(message);
-                        Application["Application_list4"] = list4;
-                        break;
-                    case 5:
-                        list5.Add(message);
-                        Application["Application_list5"] = list5;
-                        break;
-                   
-
-                    default:
-
-                        break;
-                }
-
-                //switch whose turn
-                var temp = Application["whoseturnisit"];
-                if ((string)temp == "1")
-                {
-                    Application["whoseturnisit"] = 0;
-                }
-                else
-                {
-                    Application["whoseturnisit"] = 1;
-                }
-
+                return;
 
             }
+            
+            //send code
+            //player 1 goes first
+            //this is player one's first run through
+            if (Session["MyUserNumber"] == Application["whoseturnisit"] )
+            {
+                
+                ///////////////////////////////////////////////
+                //this here is send a message, above is receive
+                ///////////////////////////////////////////////
 
+                //this is button one, really - user chose button one 
+
+                Button6.Enabled = false;
+                var holder1 = Application["randomnumber1"];
+
+                //you won got right pick
+                if (holder1.ToString() == "1")
+                {
+                    //list1.Add(a);
+                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "alert('You Win!'); ", true);
+
+                }
+
+
+                //wrong pick - send message with code to disable
+                else
+                {
+
+                    ///////////////////////////////////////////////////////////////////////
+                    //this, below, a function//////////////////////////////////////////////
+                    ///////////////////////////////////////////////////////////////////////
+                    string msg = holder1.ToString(); 
+                    OneChatRoom message = new OneChatRoom(msg, "0", "0");
+                    //list1.Add(msg1);
+
+
+                    //////////!!!!!!!!!!!!!!!!!!!!!TWO OF THESE!!!! forcing
+                    //variable set at previous screen
+                    var roomnumber = 1;
+
+
+                    //sends message
+                    switch (roomnumber)
+                    {
+                        case 1:
+                            list1.Add(message);
+                            Application["Application_list1"] = list1;
+                            break;
+
+                        case 2:
+                            list2.Add(message);
+                            Application["Application_list2"] = list2;
+                            break;
+                        case 3:
+                            list3.Add(message);
+                            Application["Application_list3"] = list3;
+                            break;
+                        case 4:
+                            list4.Add(message);
+                            Application["Application_list4"] = list4;
+                            break;
+                        case 5:
+                            list5.Add(message);
+                            Application["Application_list5"] = list5;
+                            break;
+
+
+                        default:
+
+                            break;
+                    }
+
+                    //switch whose turn
+                    var temp = Application["whoseturnisit"];
+                    if (temp == "1")
+                    {
+                        Application["whoseturnisit"] = "0";
+                    }
+                    else
+                    {
+                        Application["whoseturnisit"] = "1";
+                    }
+
+
+                }
+
+               
+            }
         }
-
         protected void Button2_Click(object sender, EventArgs e)
         {
             Button2.Enabled = false;
@@ -341,5 +367,71 @@ namespace lobby
             }
 
         }
+        //player 2
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+
+            Session["MyUserNumber"] = "0";
+        }
+        //player 1
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            Session["MyUserNumber"] = "1";
+
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            var holder = Application["randomnumber1"];
+            Label1.Text = holder.ToString();
+
+            string var = "0";
+
+            MyClass MyClass1 = new MyClass();
+
+            Application["Temp"] = "TestValue";
+            SessionTest sessionTest = new SessionTest();
+            sessionTest.GetSession(Application["Temp"].ToString());
+
+        }
     }
 }
+
+public class ReceiveCode
+{
+
+
+
+}
+
+public class SendCode
+{
+
+}
+
+
+
+public class ChangeUser
+{
+    public void MyFunction()
+    {
+        //switch whose turn
+        var temp = HttpContext.Current.Session["whoseturnisit"];
+        if (temp == "1")
+        {
+            HttpContext.Current.Session["whoseturnisit"] = "0";
+        }
+        else
+        {
+            HttpContext.Current.Session["whoseturnisit"] = "1";
+        }
+    }
+       
+}
+
+
+
+                    
+
+
+
--- lobby/Site1.Master.designer.cs
+++ lobby/Site1.Master.designer.cs
@@ -94,6 +94,69 @@ namespace lobby {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
+        /// Button7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button7;
+        
+        /// <summary>
+        /// Button8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button8;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Button9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button9;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
